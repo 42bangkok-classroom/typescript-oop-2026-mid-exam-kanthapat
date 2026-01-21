d924c92da31d9ee742b1d485839c0d5f975fb4a1
kanthapat <sailom.01216@gmail.com>
2026-01-21 15:01:25 +0700

    d

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,10 +1,24 @@
+import { measureMemory } from "node:vm";
+
 // Write your code below
 const args = process.argv[2];
 
 if(args) {
     const num = Number(args.trim());
 
-    for (let i = 1; i < ; i--) {
-        
+    if (!isNaN(num)&& num > 0) {
+        for (let i = 1; i < num+1; i++) {
+            let message = "";
+            for (let j = num; j > 0; j--){
+                if(j <= i){
+                    message = message + j;
+                } else {
+                    message = message + ' ';
+                }
+            }
+    
+            console.log(message);
+            
+        }
     }
 }
