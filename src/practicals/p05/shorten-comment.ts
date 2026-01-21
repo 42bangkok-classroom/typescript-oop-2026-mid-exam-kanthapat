941c4d4736679f6bdc18a5f85fbc4dc1480a4f36
kanthapat <sailom.01216@gmail.com>
2026-01-21 15:17:34 +0700

    e

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,4 +1,4 @@
-const comment = "TypeScript is a strongly typed programming language"
+// const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
   // Write your code below
