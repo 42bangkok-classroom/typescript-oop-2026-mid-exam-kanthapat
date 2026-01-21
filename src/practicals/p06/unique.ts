94b04b51b908d39ea6446309273c8e8fc025e102
kanthapat <sailom.01216@gmail.com>
2026-01-21 15:31:07 +0700

    f

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -13,4 +13,4 @@ function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
 
-console.log(getUniqueNumbers(arr1, arr2));
+// console.log(getUniqueNumbers(arr1, arr2));
