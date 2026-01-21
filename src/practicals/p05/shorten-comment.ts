92e64275e3131f74d70c077c57194e1f728d7667
kanthapat <sailom.01216@gmail.com>
2026-01-21 15:15:32 +0700

    e

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -2,4 +2,21 @@ const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
   // Write your code below
+  let result = "";
+  const text = comment.split(' ');
+  for (const item of text) {
+    if (item.length >= 5 && item.length <= 10) {
+      if (result) {
+        result = result + ' ' + item;
+      } else {
+        result = result + item;
+      }
+      
+    }
+  }
+
+  return result;
+  // console.log(text[0])
 }
+
+// console.log(shortenComment(comment));
